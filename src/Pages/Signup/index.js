bff32acd5fe209bc5e6731864775f3fb11a538d5
marirr86 <rr.mari@hotmail.com>
2021-09-23 13:44:11 -0300

    router-dom

---
+++ src/Pages/Signup/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div
+            style={{
+                height: '100vh',
+                width: '100%',
+                background: 'red',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+            }}
+        >
+            Cadastro
+        </div>    
+    );
+}
+
+export default Signup
